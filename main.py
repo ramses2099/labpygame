46816ff14da31c3330747e924aa650f251eea7ed
ramses2099 <ramses2099@gmail.com>
2023-06-19 06:15:33 -0400

    changes in the main file

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
 import pygame
 
+TITLE = "TEST GAME"
+WINDOW_SIZE = (1280, 720)
+FPS = 60
+BLACK = (0, 0, 0)
+RED = (255, 30, 70)
+BLUE = (10, 20, 200)
+GREEN = (50, 230, 40)
+WHITE = (255, 255, 255)
+
 
 class App:
     def __init__(self):
         # pygame setup
         pygame.init()
-        self.screen = pygame.display.set_mode((1280, 720))
+        self.screen = pygame.display.set_mode(WINDOW_SIZE)
+        pygame.display.set_caption(TITLE)
         self.clock = pygame.time.Clock()
         self.running = True
 
     def run(self):
-        pass
+        while self.running:
+            # poll for events
+            # pygame.QUIT event means the user clicked X to close your window
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            # fill the screen with a color to wipe away anything from last frame
+            self.screen.fill(BLACK)
+
+            # RENDER YOUR GAME HERE
+
+            # flip() the display to put your work on screen
+            pygame.display.flip()
+
+            self.clock.tick(60)  # limits FPS to 60
+
+        pygame.quit()
 
 
 if __name__ == "__main__":
